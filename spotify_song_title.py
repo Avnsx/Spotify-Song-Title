e1a4594b2330320bd29c34b77a53511797fd3024
Mika C <59329994+Avnsx@users.noreply.github.com>
2021-06-25 11:50:00 +0200

    performance & functionality improvements
    + changed the way it finds spotify(by pid), you won't need to blacklist program names anymore & also it's way quicker now since it doesn't try to read the song title from the application itsself but actually just uses the window title. this should be as lightweight as possible 🎉

--- spotify_song_title.py
+++ spotify_song_title.py
@@ -1,21 +1,21 @@
-import pywinauto
+import pywinauto,psutil,random
 from pywinauto.application import Application
 from pywinauto import Desktop
 from time import sleep as s
 
-blacklist=['Google','Chrome','Sublime','Discord','obs64','OBS','Taskleiste','Taskbar','Firefox','Opera','Edge','Blitz','Lautstärkemixer','File','Explorer','Program','Manager','League','Launcher']
-
 while True:
-	window_titles=[w.window_text() for w in Desktop(backend="uia").windows()]
-	best_guess = [f for f in window_titles if all([word not in f for word in blacklist])]
-	for x in range (0,len(best_guess)):
-		try:
-			app_controls = Application(backend="uia").connect(title=best_guess[x])[best_guess[x]]
-			split_s=best_guess[x].split(' - ')
-			if split_s[1] or split_s[0] in str(app_controls.GroupBox.texts()[0]):
-				song_name=str('Song: '+split_s[1]+' by '+split_s[0]+' ♪')
-				print(song_name)
-				with open('song_name.txt', 'w', encoding="utf-8") as output:output.write(song_name)
-				s(10)
-				break
-		except:pass
+    try:
+        mylist=[]
+        for proc in psutil.process_iter():
+            if proc.name() == 'Spotify.exe':
+                mylist.append(proc.as_dict(attrs=['pid','num_threads'])) #windows only: 'num_handles'
+        song_name_raw=[w.window_text() for w in Desktop(backend='uia').windows(process=int(max(mylist, key=lambda x: x['num_threads'])['pid']))][0]
+    except:
+        input('Spotify not found; Press Enter once you have started Spotify back up...')
+        continue
+    if song_name_raw not in ('Spotify Free', 'Spotify Premium'):
+        song_name=str('Song: '+song_name_raw+' ♪')
+        print(song_name)
+        with open('song_name.txt', 'w', encoding='utf-8') as o:o.write(song_name)
+    s(random.uniform(8,15))
+
